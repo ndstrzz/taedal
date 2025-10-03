ea6aeeb07840878b0080df20f9b85f548be26132
ndstrzz <andynilessim05@gmail.com>
2025-10-03 12:32:34 +0800

    trying the api base change to render http

--- client/public/config.js
+++ client/public/config.js
@@ -1,5 +1,5 @@
 // client/public/config.js
 window.__CONFIG__ = {
-  API_BASE: "https://taedal.netlify.app",
+  API_BASE: "https://taedal.onrender.com",
   FRONTEND_BASE: "https://taedal.netlify.app" // or leave undefined to default to window.location.origin
 };
