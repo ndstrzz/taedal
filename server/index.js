96efcb2a0aa65e08b4bd224cc8a07abb1436642c
ndstrzz <andynilessim05@gmail.com>
2025-10-03 12:28:18 +0800

    CHANGING THE REACT APP API BASE VALUES"

--- server/index.js
+++ server/index.js
@@ -1,1 +1,33 @@
+// install: npm i cors
+const cors = require("cors");
+
+const ALLOWED_ORIGIN =
+  process.env.CORS_ORIGIN ||
+  process.env.FRONTEND_BASE ||           // e.g. https://taedal.netlify.app
+  "http://localhost:3000";
+
+app.set("trust proxy", 1);               // important for secure cookies on Render
+
+app.use(cors({
+  origin: ALLOWED_ORIGIN,
+  credentials: true,
+  methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization","X-Requested-With"],
+}));
+
+// Respond to preflight quickly
+app.options("*", cors({
+  origin: ALLOWED_ORIGIN,
+  credentials: true,
+}));
+
+app.use((req, res, next) => {
+  // allow Google OAuth popup postMessage
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+  // not required, but avoids COEP warnings
+  res.setHeader("Cross-Origin-Embedder-Policy", "unsafe-none");
+  next();
+});
+
+
 module.exports = require('./src/index');
